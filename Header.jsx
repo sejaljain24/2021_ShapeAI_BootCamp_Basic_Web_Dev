e9ec5f6e9ce72eac4005c969d1620ed85a54ff1c
Sejal Jain <71178616+sejaljain24@users.noreply.github.com>
2021-06-05 12:31:42 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>2021 BootCamp - JavaScript & React.js</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>
+        <mark>JavaScript and React.js</mark>
+      </h1>
+      <p>
+        This was an amazing bootcamp taken by our mentor{" "}
+        <em>
+          <strong>"Shaurya Sinha"</strong>
+        </em>
+        . We covered everything from scratch. And learned so many things.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
